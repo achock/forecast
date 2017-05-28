b2e6a5903fa3f93fda11d54b5ee8e32169d1d9af
achock <achock@users.noreply.github.com>
2017-05-28 17:57:39 -0600

    Delete config.py

--- instance/config.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY= 'p9Bv<3Eid9%$i01'
-
-#SQLALCHEMY_DATABASE_URI = 'mysql://mUsername:mPassword@localhost/forecast_db'
-SQLALCHEMY_DATABASE_URI = 'mysql://us-cdbr-iron-east-03.cleardb.net/ad_e5cfd6bb5b9a7b6?user=ba88b61ac093ce&password=933c79a7'
